de94261fa00ba5b18ed0fabb3fbf2ecf6e9e0bed
Lutfi Fadil Lokman <47475707+LutfiLokman@users.noreply.github.com>
2025-04-07 14:36:25 -0700

    auto-push

--- plugin/seekdeep.lua
+++ plugin/seekdeep.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_create_user_command("HelloWorld", function()
-  require("helloworld").say_hello()
+  require("seekdeep").say_hello()
 end, { desc = "Say Hello World in a floating window" })
